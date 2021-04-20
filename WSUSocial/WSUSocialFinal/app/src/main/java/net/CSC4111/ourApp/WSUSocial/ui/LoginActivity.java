07478d4a68bd27ad69ebb72493e761a3b2c13184
ibe-harb <gh6499@wayne.edu>
2021-04-19 21:37:20 -0400

    Mhm

--- WSUSocial/WSUSocialFinal/app/src/main/java/net/CSC4111/ourApp/WSUSocial/ui/LoginActivity.java
+++ WSUSocial/WSUSocialFinal/app/src/main/java/net/CSC4111/ourApp/WSUSocial/ui/LoginActivity.java
@@ -1,5 +1,4 @@
 package net.CSC4111.ourApp.WSUSocial.ui;
-
 import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
@@ -18,110 +17,14 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import net.CSC4111.ourApp.WSUSocial.R;
-
-public class LoginActivity extends AppCompatActivity {
-    private EditText mInputEmail, mInputPassword;
-    private FirebaseAuth mAuth;
-    private ProgressBar mProgressBar;
-    private Button mBtnRegister, mBtnLogin;
-
+public class LoginActivity extends AppCompatActivity{private EditText mInputEmail, mInputPassword;private FirebaseAuth mAuth;private ProgressBar mProgressBar;private Button mBtnRegister, mBtnLogin;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        initializeFirebase();
-        // set the view now
-        setContentView(R.layout.activity_login);
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        findViews();
-        setOnClickMethods();
-    }
-
-    private void initializeFirebase() {
-        mAuth = FirebaseAuth.getInstance();
-        if (mAuth.getCurrentUser() != null) {
-            startActivity(new Intent(LoginActivity.this, MainActivity.class));
-            finish();
-        }
-    }
-
-    private void findViews() {
-        mInputEmail = (EditText) findViewById(R.id.email);
-        mInputPassword = (EditText) findViewById(R.id.password);
-        mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);
-        mBtnRegister = (Button) findViewById(R.id.btn_signup);
-        mBtnLogin = (Button) findViewById(R.id.btn_login);
-    }
-
-    private void setOnClickMethods() {
-        mBtnRegister.setOnClickListener(new View.OnClickListener() {
+    protected void onCreate(Bundle savedInstanceState){super.onCreate(savedInstanceState);initializeFirebase();setContentView(R.layout.activity_login);Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);setSupportActionBar(toolbar);findViews();setOnClickMethods();}
+    private void initializeFirebase(){mAuth = FirebaseAuth.getInstance();if (mAuth.getCurrentUser() != null){startActivity(new Intent(LoginActivity.this, MainActivity.class));finish();}}
+    private void findViews(){mInputEmail = (EditText) findViewById(R.id.email);mInputPassword = (EditText) findViewById(R.id.password);mProgressBar = (ProgressBar) findViewById(R.id.progress_bar);mBtnRegister = (Button) findViewById(R.id.btn_signup);mBtnLogin = (Button) findViewById(R.id.btn_login);}private void setOnClickMethods(){mBtnRegister.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
-            }
-        });
-
-        mBtnLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){startActivity(new Intent(LoginActivity.this, RegisterActivity.class));}});mBtnLogin.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                String email = mInputEmail.getText().toString();
-                final String password = mInputPassword.getText().toString();
-
-                if (TextUtils.isEmpty(email)) {
-                    mInputEmail.setError(getString(R.string.error_toast_enter_email));
-                    Toast.makeText(getApplicationContext(),
-                            getString(R.string.error_toast_enter_email),
-                            Toast.LENGTH_SHORT).show();
-                    return;
-                }
-                if (!android.util.Patterns.EMAIL_ADDRESS.matcher(mInputEmail.getText().toString()).matches()){
-                    mInputEmail.setError(getString(R.string.wrong_email_format));
-                    return;
-                }
-
-                if (TextUtils.isEmpty(password)) {
-                    Toast.makeText(getApplicationContext(),
-                            getString(R.string.error_toast_enter_password),
-                            Toast.LENGTH_SHORT).show();
-                    return;
-                }
-
-                mProgressBar.setVisibility(View.VISIBLE);
-                mBtnRegister.setEnabled(false);
-                mBtnLogin.setEnabled(false);
-                //authenticate user
-                mAuth.signInWithEmailAndPassword(email, password)
-                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+            public void onClick(View v){String email = mInputEmail.getText().toString();final String password = mInputPassword.getText().toString();if(TextUtils.isEmpty(email)){mInputEmail.setError(getString(R.string.error_toast_enter_email));Toast.makeText(getApplicationContext(), getString(R.string.error_toast_enter_email),Toast.LENGTH_SHORT).show();return;}if (!android.util.Patterns.EMAIL_ADDRESS.matcher(mInputEmail.getText().toString()).matches()){mInputEmail.setError(getString(R.string.wrong_email_format));return;}if (TextUtils.isEmpty(password)){Toast.makeText(getApplicationContext(),getString(R.string.error_toast_enter_password),Toast.LENGTH_SHORT).show();return;}mProgressBar.setVisibility(View.VISIBLE);mBtnRegister.setEnabled(false);mBtnLogin.setEnabled(false);mAuth.signInWithEmailAndPassword(email, password) .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>(){
                             @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                // If sign in fails, display a message to the user. If sign in succeeds
-                                // the auth state listener will be notified and logic to handle the
-                                // signed in user can be handled in the listener.
-                                mProgressBar.setVisibility(View.GONE);
-                                mBtnRegister.setClickable(true);
-                                mBtnLogin.setClickable(true);
-                                mBtnRegister.setEnabled(true);
-                                mBtnLogin.setEnabled(true);
-                                if (!task.isSuccessful()) {
-                                    // there was an error
-                                    if (password.length() < RegisterActivity.MIN_PASSWRD_LENGTH) {
-                                        mInputPassword.setError(getString(R.string.minimum_password));
-                                    } else {
-                                        Toast.makeText(LoginActivity.this,
-                                                getString(R.string.auth_failed),
-                                                Toast.LENGTH_LONG).show();
-                                    }
-                                } else {
-                                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                                    startActivity(intent);
-                                    finish();
-                                }
-                            }
-                        });
-            }
-        });
-    }
-}
+                            public void onComplete(@NonNull Task<AuthResult> task){mProgressBar.setVisibility(View.GONE);mBtnRegister.setClickable(true);mBtnLogin.setClickable(true);mBtnRegister.setEnabled(true);mBtnLogin.setEnabled(true);if (!task.isSuccessful()){if (password.length() < RegisterActivity.MIN_PASSWRD_LENGTH){mInputPassword.setError(getString(R.string.minimum_password));}else{Toast.makeText(LoginActivity.this,getString(R.string.auth_failed),Toast.LENGTH_LONG).show();}}else{Intent intent = new Intent(LoginActivity.this, MainActivity.class);startActivity(intent);finish();}}});}});}}
